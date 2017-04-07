ee66d1b618dc2c9fc4418e0770c12e0cdedfa12d
jeevan kp <jeevan.kp@hp.com>
2017-04-07 17:41:09 +0530

    frist commit

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {todoAppComponent} from './todo.component';
+
+//calling bootstrap function with AppCompnent as parameter
+bootstrap(todoAppComponent);
